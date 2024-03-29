c36e7886f89e21fe30c092a43ae26ecf2d70d096
poping520 <poping52096@gmail.com>
2024-03-29 17:29:55 +0800

    Add PPU

---
+++ src/common.h
@@ -0,0 +1,10 @@
+//
+// Created by WangKZ on 2024/3/29.
+//
+
+#ifndef WINES_COMMON_H
+#define WINES_COMMON_H
+
+#include <stdint.h>
+
+#endif //WINES_COMMON_H
---
+++ src/ppu.h
@@ -0,0 +1,11 @@
+//
+// Created by WangKZ on 2024/3/28.
+//
+
+#ifndef WINES_PPU_H
+#define WINES_PPU_H
+
+#include "common.h"
+
+
+#endif //WINES_PPU_H
